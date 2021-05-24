9ebf0fd7ff487dc5fde72c88df57556f01add7c2
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-05-24 20:36:26 -0300

    Aula 03 Spring Boot

---
+++ aula03_springboot_config_yaml/src/main/java/br/com/feitosa/jessica/aula02_springboot_config_yaml/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.feitosa.jessica.aula02_springboot_config_yaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
